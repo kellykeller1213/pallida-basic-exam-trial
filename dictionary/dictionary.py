9f771519aceb4c5da4011252057c335419a47c8e
kellykeller1213 <keller.f.peter@gmail.com>
2017-09-19 12:55:38 +0200

    feat(dictionary): first user input works but doesnt print

--- dictionary/dictionary.py
+++ dictionary/dictionary.py
@@ -7,16 +7,20 @@ dictionary = [
 # list 'dictionary'
 
 
-def add_word(hun_word, eng_word):
-    pass
+def add_word():
+    hun_word = input("Kérlek írd be az előző szó jelentését magyar nyelven: ")
+    eng_word = input("Please type in a word: ")
+    dictionary[hun_word] = eng_word
+    print(dictionary)
 
+add_word()
 # Implement these methods. They should return the translation of the given
 # word form the list 'dictionary'
 
 
-def translate_to_hun(eng_word):
-    pass
+#def translate_to_hun(eng_word):
+  #  pass
 
 
-def translate_to_eng(hun_word):
-    pass
+#def translate_to_eng(hun_word):
+ #   pass
