fc238267aedced65144909890063143df3b7c57b
peter-keller <keller.f.peter@gmail.com>
2018-01-23 18:21:35 +0100

    feat(dictionary): finished version

--- dictionary/dictionary.py
+++ dictionary/dictionary.py
@@ -3,24 +3,44 @@ dictionary = [
     {"fa": "tree"}
 ]
 
+
 # Implement this method. It should add the given key-value pair to the
 # list 'dictionary'
 
 
-def add_word():
-    hun_word = input("Kérlek írd be az előző szó jelentését magyar nyelven: ")
-    eng_word = input("Please type in a word: ")
-    dictionary[hun_word] = eng_word
-    print(dictionary)
+def add_word(hun, eng):
+    dictionary.append({hun: eng})
 
-add_word()
+
 # Implement these methods. They should return the translation of the given
 # word form the list 'dictionary'
 
 
-#def translate_to_hun(eng_word):
-  #  pass
+def key_list_maker():
+    key_list = []
+    for i in range(len(dictionary)):
+        key_list += [(list(dictionary[i].keys())[0])]
+    return key_list
 
 
-#def translate_to_eng(hun_word):
- #   pass
+def translate_to_hun(eng_word):
+    keys = key_list_maker()
+    i = 0
+    for line in dictionary:
+        if line[keys[i]] == eng_word:
+            return keys[i]
+        i += 1
+
+
+def translate_to_eng(hun_word):
+    keys = key_list_maker()
+    i = 0
+    for line in dictionary:
+        if keys[i] == hun_word:
+            return line[keys[i]]
+        i += 1
+
+
+add_word("asztal", "desk")
+print(translate_to_hun("desk"))
+print(translate_to_eng("asztal"))
